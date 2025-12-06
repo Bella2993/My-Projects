c2a64fd3faafb59e56a723ac7f9d474f92b22f5f
Belinda Aidoo <126023392+Bella2993@users.noreply.github.com>
2025-12-06 15:16:47 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+var path = window.location.pathname;
+
+var links = document.querySelectorAll('nav a');
+
+
+links.forEach(function(link) {
+  if (link.getAttribute('href') === path) {
+    link.classList.add('active');
+  }
+});
